ad9eeb3f2bd36d347e8757c0131094557d62a401
Vladimir GRYSHCHENKO <vgryshch@e1r3p6.unit.ua>
2018-07-05 10:43:56 +0300

    remove redux-form

--- resources/assets/js/components/Signup/Signup.jsx
+++ resources/assets/js/components/Signup/Signup.jsx
@@ -11,7 +11,7 @@ class Signup extends Component  {
           <h5>Sign up</h5>
           <Input s={6} label="First Name" />
           <Input s={6} label="Last Name" />
-          <Input label="Login" s={6} />
+          <Input name="username" label="Username" s={6} />
           <Input type="email" label="Email" s={6} />
           <Input type="password" label="Password" s={12} />
           <div className="col input-field s12">
