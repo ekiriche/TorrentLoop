a8bd0d2339479c73377542cceacea52ae0449d68
Dornat <DjDornat1@mail.ru>
2018-07-05 15:52:29 +0300

    added forgot and update pass

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -9,24 +9,13 @@ use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     public function attemptLogin(Request $request)
     {
-      $user = User::where('login', $request->input('login'))->first();
-      if ($user == '')
-        return "User not found";
-      if (Hash::check($request->input('password'), $user->password))
-        return "OK";
-      return "Password is wrong";
+        $user = User::where('login', $request->input('login'))->first();
+        if ($user == '')
+            return "User not found";
+        if (Hash::check($request->input('password'), $user->password))
+            return "OK";
+        return "Password is wrong";
     }
 }
