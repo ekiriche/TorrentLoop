6901853dda34d327903fc4d2e04deceab58049c5
Gryshchenko <Gryshchenko.89@gmail.com>
2018-07-09 14:37:56 +0300

    style Footer @media

--- resources/assets/js/components/Footer/Footer.jsx
+++ resources/assets/js/components/Footer/Footer.jsx
@@ -10,11 +10,13 @@ class Foot extends Component {
 			<footer className="page-footer">
 				<div className="footer-copyright">
 				  <div className="container footer-text-position">
-						© 2018 <Translate id="footer">Created by</Translate>
-					  <a className="grey-text text-lighten-4 right creat-margin" target="_blank" href="https://github.com/Dornat">dpolosuh</a>
-						<a className="grey-text text-lighten-4 right creat-margin" target="_blank" href="https://github.com/Madranko">mmotov</a>
-						<a className="grey-text text-lighten-4 right creat-margin" target="_blank" href="http://www.github.com/Gryshchenko">vgryshch</a>
-						<a className="grey-text text-lighten-4 right creat-margin" target="_blank" href="http://www.github.com/ekiriche">ekiriche</a>
+						<ul className="creatBy-flex">
+							<li>© 2018 <Translate id="footer">Created by</Translate></li>
+							<li><a className="grey-text text-lighten-4 right creat-margin" target="_blank" href="https://github.com/Dornat">dpolosuh</a></li>
+							<li><a className="grey-text text-lighten-4 right creat-margin" target="_blank" href="https://github.com/Madranko">mmotov</a></li>
+							<li><a className="grey-text text-lighten-4 right creat-margin" target="_blank" href="http://www.github.com/Gryshchenko">vgryshch</a></li>
+							<li><a className="grey-text text-lighten-4 right creat-margin" target="_blank" href="http://www.github.com/ekiriche">ekiriche</a></li>
+						</ul>
 					</div>
 				</div>
 			</footer>
