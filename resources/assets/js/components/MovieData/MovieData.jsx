22e3e9d3b95f4e0deb962b981ab3a2f185d1fb88
Madranko <motov.maxim@gmail.com>
2018-08-02 11:23:51 +0300

    movie history part 2, please do not delete NOTHING from MovieData

--- resources/assets/js/components/MovieData/MovieData.jsx
+++ resources/assets/js/components/MovieData/MovieData.jsx
@@ -33,11 +33,23 @@ class MovieData extends Component  {
 	componentWillMount(){
 		let jwt = localStorage.getItem('accessToken');
 		let user = jwtDecode(jwt);
-		axios.post('http://localhost:8100/auth/token-update', {'id' : user.uid, 'jwt' : jwt}).then (result => {
-			if (result.data == 'expired')
-				localStorage.removeItem('accessToken');
+		PostData('profile/save-history', {
+			'movie_id': this.state.movie.id,
+			'imdb_code': this.state.movie.imdb_code,
+			'medium_cover_image': this.state.movie.medium_cover_image,
+			'title_english': this.state.movie.title_english,
+			'year': this.state.movie.year,
+			'rating': this.state.movie.rating,
+			'jwt': jwt
+		}).then ((result) => {
+			console.log(result);
 		});
-		console.log("result");
+		// PostData('http://localhost:8100/auth/token-update', {'id' : user.uid, 'jwt' : jwt}).then (result => {
+		// 	if (result.data == 'expired')
+		// 	localStorage.removeItem('accessToken');
+		// 	console.log(this.state.movie.id);
+        //
+		// });
 	}
 
 	startDownload(event) {
@@ -52,18 +64,18 @@ class MovieData extends Component  {
 			'rating': this.state.movie.rating,
 			'user_id': user.uid
 		}).then ((result) => {
-			console.log(result);
+			// console.log(result);
 		});
 		setTimeout(function() {
-		PostData('movie/download-movie', { 'imdb-id': this.state.movie.imdb_code }).then ((moviepath) => {
-			console.log(moviepath);
-			this.setState({ moviePath : moviepath });
-			this.setState({ download : true });
-	/*		setTimeout(function() {
+			PostData('movie/download-movie', { 'imdb-id': this.state.movie.imdb_code }).then ((moviepath) => {
+				// console.log(moviepath);
+				this.setState({ moviePath : moviepath });
+				this.setState({ download : true });
+				/*		setTimeout(function() {
 				PostData('movie/download-subtitles', { 'imdb-id': this.state.movie.imdb_code }).then ((result) => {
-					this.setState({ subtitles: result });
-					this.setState({ download : true });
-					})
+				this.setState({ subtitles: result });
+				this.setState({ download : true });
+				})
 				}.bind(this), 7000); */
 			})
 		}.bind(this), 7000);
@@ -96,9 +108,9 @@ class MovieData extends Component  {
 
 	const videoQuality = this.state.movie.torrents
 	const videoQualityList = videoQuality.map((size, i) =>
-		<li key={i}>
-			<a className="waves-effect waves-light btn" onClick={this.startDownload} id={size.quality}>
-				<i className="material-icons left" >cloud_download</i>{size.quality}</a>
+	<li key={i}>
+		<a className="waves-effect waves-light btn" onClick={this.startDownload} id={size.quality}>
+			<i className="material-icons left" >cloud_download</i>{size.quality}</a>
 		</li>
 	)
 
@@ -107,34 +119,34 @@ class MovieData extends Component  {
 		<Col m={7} s={12}>
 			<Card horizontal header={<CardTitle image={this.state.movie.large_cover_image}></CardTitle>}>
 				<div className="MovieData-position">
-						<h5>{this.state.movie.title}</h5>
-						<h6>Discription</h6>
-						{this.state.movie.description_full}
-						<h6>Genres</h6>
-						<div className="ganres-list">
-							{listGenres}
-						</div>
-						<h6>Rating</h6>
-						{this.state.movie.rating}
-						<h6>Time</h6>
-						{this.state.movie.runtime}
-						<h6>Year</h6>
-						{this.state.movie.year}
-						<div className="videoQuality">
-							{(!this.state.download) ? videoQualityList : null}
-						</div>
+					<h5>{this.state.movie.title}</h5>
+					<h6>Discription</h6>
+					{this.state.movie.description_full}
+					<h6>Genres</h6>
+					<div className="ganres-list">
+						{listGenres}
+					</div>
+					<h6>Rating</h6>
+					{this.state.movie.rating}
+					<h6>Time</h6>
+					{this.state.movie.runtime}
+					<h6>Year</h6>
+					{this.state.movie.year}
+					<div className="videoQuality">
+						{(!this.state.download) ? videoQualityList : null}
+					</div>
 				</div>
 			</Card>
 			<Comments data={this.state.movie}/>
 		</Col>
 	);
-	}
+}
 }
 export default withLocalize(MovieData);
 /*
 {(!this.state.download)
-	? <a className="waves-effect waves-light btn" onClick={this.startDownload}><i className="material-icons left">cloud_download</i>Watch</a>
-	: <a className="btn disabled" ><i className="material-icons left">cloud_download</i>Watch</a>}
+? <a className="waves-effect waves-light btn" onClick={this.startDownload}><i className="material-icons left">cloud_download</i>Watch</a>
+: <a className="btn disabled" ><i className="material-icons left">cloud_download</i>Watch</a>}
 {(this.state.download)
 ? <div className="progress percentLoader"><div className="determinate" style={{width: this.state.downloadPercent + '%'}}></div></div>
 : null}
