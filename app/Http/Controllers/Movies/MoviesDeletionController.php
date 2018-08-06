8427a3532d0ed3052b7822d4852b90070f9601ae
Madranko <motov.maxim@gmail.com>
2018-08-06 14:28:53 +0300

    stream is working a little better

--- app/Http/Controllers/Movies/MoviesDeletionController.php
+++ app/Http/Controllers/Movies/MoviesDeletionController.php
@@ -9,47 +9,55 @@ use App\Film;
 class MoviesDeletionController extends Controller
 {
 
-  public function removeDirectory($dir)
-  {
-    $files = array_diff(scandir($dir), array('.','..'));
-    foreach ($files as $file) {
-      is_dir("$dir/$file") ? $this->removeDirectory("$dir/$file") : unlink("$dir/$file");
-    }
-    return rmdir($dir);
+	public function removeDirectory($dir)
+	{
+		$files = array_diff(scandir($dir), array('.','..'));
+		foreach ($files as $file) {
+			is_dir("$dir/$file") ? $this->removeDirectory("$dir/$file") : unlink("$dir/$file");
+		}
+		return rmdir($dir);
 
-  }
+	}
 
-  public function addFilmToDB(Request $request)
-  {
-    $imdb_id = $request->input('imdb-id');
-    $film = Film::where('imdb_id', $imdb_id)->first();
-    if ($film == '')
-    {
-      Film::create([
-        'imdb_id' => $imdb_id
-      ]);
-    }
-    else
-    {
-      $film->fill([
-        'updated_at' => now()
-      ])->save();
-    }
-    return "OK";
-  }
+	public function addFilmToDB(Request $request)
+	{
+		// return $request->input('path');
+		$path = $request->input('path');
+		$delete_time = $request->input('timeToDelete');
+		$film = Film::where('path', $path)->first();
+		if ($film == '')
+		{
+			Film::create(
+				[
+					'path' => $path,
+					'delete_time' => $delete_time
+				]
+			);
+		}
+		else
+		{
+			$film->fill(
+				[
+					'delete_time' => $delete_time
+				]
+			)->save();
+		}
+		return "OK";
+	}
 
-  public function deleteNotWatchedFilms(Request $request)
-  {
-    $films = Film::all();
-    foreach($films as $key => $item)
-    {
-      if (now()->timestamp - $item->updated_at->timestamp >= 2592000)
-      {
-        if (is_dir("torrent-stream/public/downloaded_movies/" . $item->imdb_id))
-          $this->removeDirectory("torrent-stream/public/downloaded_movies/" . $item->imdb_id);
-        $item->delete();
-      }
-    }
-    return "OK";
-  }
+	public function deleteNotWatchedFilms(Request $request)
+	{
+		$films = Film::all();
+		foreach($films as $key => $item)
+		{
+			if (now()->timestamp - $item->updated_at->timestamp >= 2592000)
+			{
+				if (is_dir("torrent-stream/public/downloaded_movies/" . $item->imdb_id)) {
+					$this->removeDirectory("torrent-stream/public/downloaded_movies/" . $item->imdb_id);
+				}
+				$item->delete();
+			}
+		}
+		return "OK";
+	}
 }
